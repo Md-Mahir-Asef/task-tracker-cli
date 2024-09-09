baf78410771428d4bd677518b24d69d2a80f516c
Md. Mahir Asef <mdmahirasef2008@gmail.com>
2024-09-09 12:33:59 +0600

    Read operation is intigrated. list.js file is created and developed.

--- bin/utilities/list.js
+++ bin/utilities/list.js
@@ -4,9 +4,39 @@ var data = fs.readFileSync("./data.json");
 var data = JSON.parse(data);
 
 const list = (args) => {
-    if(args[2] == "list"){
-        console.log(data.tasks);
+  if (args[2] == "list") {
+    console.log("\n-------------------------------------------------");
+    const status = args[3];
+    if (
+      !(
+        status ||
+        status == "todo" ||
+        status == "in-progress" ||
+        status == "done" ||
+        status == "N/A"
+      )
+    ) {
+      for (let i = 0; i < data.tasks.length; i++) {
+        console.log(`ID: ${data.tasks[i].id}`);
+        console.log(`Title: ${data.tasks[i].title}`);
+        console.log(`Description: ${data.tasks[i].description}`);
+        console.log(`Status: ${data.tasks[i].status}`);
+        console.log(`Date of Creation: ${data.tasks[i].createdAt}`);
+        console.log("-------------------------------------------------");
+      }
+    } else {
+      for (let i = 0; i < data.tasks.length; i++) {
+        if (data.tasks[i].status == status) {
+          console.log(`ID: ${data.tasks[i].id}`);
+          console.log(`Title: ${data.tasks[i].title}`);
+          console.log(`Description: ${data.tasks[i].description}`);
+          console.log(`Status: ${data.tasks[i].status}`);
+          console.log(`Date of Creation: ${data.tasks[i].createdAt}`);
+          console.log("-------------------------------------------------");
+        }
+      }
     }
-}
+  }
+};
 
 module.exports = list;
